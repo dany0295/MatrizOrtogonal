a07ef08c1cfaf69145358bfcc776d496121d7446
Gemis Daniel Guevara Villeda <gemisdguevarav@gmail.com>
2018-05-03 23:58:05 -0600

    Se modificó un archivo del formulario principal anterior, se le cambió el nombre a frmPrueba.

--- src/matrizortogonal/frmPrueba.java
+++ src/matrizortogonal/frmPrueba.java
@@ -11,12 +11,12 @@ import Formularios.frmAcercaDe;
  *
  * @author darkparadox
  */
-public class frmPricipal extends javax.swing.JFrame {
+public class frmPrueba extends javax.swing.JFrame {
 
     /**
      * Creates new form frmPricipal
      */
-    public frmPricipal() {
+    public frmPrueba() {
         initComponents();
         setLocationRelativeTo(null);
     }
@@ -179,20 +179,21 @@ public class frmPricipal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmPricipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmPrueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmPricipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmPrueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmPricipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmPrueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmPricipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmPrueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frmPricipal().setVisible(true);
+                new frmPrueba().setVisible(true);
             }
         });
     }
